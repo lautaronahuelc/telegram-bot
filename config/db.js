17084a1ac0e6239bfcb096a0d4464684d44881c3
lautaronahuelc <lautaronahuel.coria@gmail.com>
2025-04-09 22:18:41 -0300

    feat: add dev database

--- config/db.js
+++ config/db.js
@@ -1,9 +1,13 @@
 import mongoose from 'mongoose';
 
 const dbConnect = async () => {
+  const dbUri = process.env.NODE_ENV === 'production'
+  ? process.env.MONGO_URI_PROD
+  : process.env.MONGO_URI_DEV;
+
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('✅ MongoDB connected successfully.');
+    await mongoose.connect(dbUri);
+    console.log(`✅ MongoDB connected successfully on ${process.env.NODE_ENV}.`);
   } catch (error) {
     console.log('❌ Failed to connect to MongoDB:', error);
     process.exit(1);
